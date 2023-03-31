15add7a9d5e191c66224b17f4c8ad02a5dcb42d9
abdulhamid95 <abdulhamidhamsho1@gamil.com>
2023-03-31 03:56:38 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -12,6 +12,28 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     })
 })
 
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
+
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
+
+
 
 console.log("أهلًا بكم في متجر عربي")
 
